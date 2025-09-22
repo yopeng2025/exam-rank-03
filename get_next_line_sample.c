443e4b165a18a5979d54d8385f794fec472d2e5d
yopeng <yongjia.peng1@gmail.com>
2025-09-22 17:47:52 +0200

    Update get_next_line_sample.c

--- get_next_line_sample.c
+++ get_next_line_sample.c
@@ -11,8 +11,6 @@ char *ft_strchr(char *s, int c)
 	return NULL;
 }
 
-// Dans l'autre sens pour eviter les chevauchement memoire
-// 在另一个方向上避免内存重叠
 void *ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t i = 0;								// MODIF
